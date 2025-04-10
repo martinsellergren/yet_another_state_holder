129265947518dd8597cb8b7190071f9b0fa935a4
martinsellergren <martin.sellergren@gmail.com>
2025-04-10 17:46:01 +0200

    Don't condition setting state with mounted.
    Just do the if (mounted) check every time manually.
    Usually when setting state we use the existing state.copyWith(), and getting state isn't allowed if unmounted either, by StateNotifier.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.5
+
+* Don't condition setting state with mounted.
+
 ## 0.2.4
 
 * Clean up error log.
--- lib/src/state_holder.dart
+++ lib/src/state_holder.dart
@@ -4,13 +4,9 @@ import 'package:state_notifier/state_notifier.dart';
 class StateHolder<T> extends StateNotifier<T> {
   StateHolder(super.state);
 
+  // visible to anyone, unlike super's
   @override
   T get state => super.state;
-
-  @override
-  set state(T value) {
-    if (mounted) super.state = value;
-  }
 
   Stream<T> get startedStream => stream.startWith(state);
 }
